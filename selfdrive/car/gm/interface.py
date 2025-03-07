f8251d82996ee8c23f8acf5f0ed7672405ae01aa
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-03-07 00:23:23 -0600

    Tune

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -29,8 +29,8 @@ CAM_MSG = 0x320  # AEBCmd
 ACCELERATOR_POS_MSG = 0xbe
 
 NON_LINEAR_TORQUE_PARAMS = {
-  CAR.CHEVROLET_BOLT_EUV: {"left": [2.2, 0.81, 0.175, -0.075], "right": [2.2, 0.81, 0.19, -0.075]},
-  CAR.CHEVROLET_BOLT_CC: {"left": [2.2, 0.81, 0.175, -0.075], "right": [2.2, 0.81, 0.19, -0.075]},
+  CAR.CHEVROLET_BOLT_EUV: {"left": [2.25, 0.81, 0.165, -0.075], "right": [2.25, 0.81, 0.195, -0.075]},
+  CAR.CHEVROLET_BOLT_CC: {"left": [2.25, 0.81, 0.165, -0.075], "right": [2.25, 0.81, 0.195, -0.075]},
   CAR.GMC_ACADIA: {"left": [4.78, 1.0, 0.31, 0.05], "right": [4.78, 1.0, 0.31, 0.05]},
   CAR.CHEVROLET_SILVERADO: {"left": [3.30, 1.0, 0.25, 0.05], "right": [3.30, 1.0, 0.25, 0.05]}
 }
