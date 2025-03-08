e22cc9246fd4c5e7a551bd9e00e2b7f760b8ee65
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-03-08 17:28:26 -0600

    Revert "Update frogpilot_acceleration.py"
    This reverts commit d5f8ee13cfc62d25fa715fe9b383cabb30bc0c53.

--- selfdrive/frogpilot/controls/lib/frogpilot_acceleration.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_acceleration.py
@@ -11,7 +11,7 @@ A_CRUISE_MIN_SPORT = A_CRUISE_MIN * 2
                        # MPH = [0.0,  11,  22,  34,  45,  56,  89]
 A_CRUISE_MAX_BP_CUSTOM =       [0.0,  5., 10., 15., 20., 25., 40.]
 A_CRUISE_MAX_VALS_ECO =        [2.0, 1.5, 1.0, 0.8, 0.6, 0.4, 0.2]
-A_CRUISE_MAX_VALS_SPORT =      [1.6, 1.75, 1.5, 1.5, 1.6, 1.6, 2.0]
+A_CRUISE_MAX_VALS_SPORT =      [1.5, 1.5, 1.25, 1.5, 1.5, 1.5, 2.0]
 A_CRUISE_MAX_VALS_SPORT_PLUS = [2.5, 2.5, 3.0, 2.5, 2.5, 2.5, 2.5]
 
 def get_max_accel_eco(v_ego):
