3a65d773516841543c3fb5b30ab415c27f5f0da7
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-07 22:24:20 -0500

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -93,10 +93,7 @@ class CarController(CarControllerBase):
     else: 
       scaled_accel = accel
     pedal_gas = clip(pedaloffset + scaled_accel * 0.6, 0.0, 1.0)
-
-    accel_cutoff = -0.5 * gain
-    if accel < accel_cutoff:
-      pedal_gas = 0.0
+    
     return pedal_gas, press_regen_paddle
 
 
