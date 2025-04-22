7ca28a35204f9a7ae4593e848823c770ebc353c2
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-22 15:47:45 -0500

    25hz

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -114,14 +114,8 @@ class CarController(CarControllerBase):
     # Send CAN commands.
     can_sends = []
 
-    # Send regen paddle and PRNDL2 commands at 40Hz using alternating 2/3 frame interval
-    frames_since_last = self.frame - getattr(self, "last_trigger_frame_40hz", -4)
-    target_wait = 3 if getattr(self, "wait_long_40hz", False) else 2
-
-    if frames_since_last >= target_wait:
-      self.last_trigger_frame_40hz = self.frame
-      self.wait_long_40hz = not getattr(self, "wait_long_40hz", False)
-
+    # Send regen paddle and PRNDL2 commands at 25Hz
+    if self.frame % 4 == 0:
       regen_active = (
         self.CP.carFingerprint in CC_REGEN_PADDLE_CAR and
         self.CP.openpilotLongitudinalControl and
@@ -129,19 +123,16 @@ class CarController(CarControllerBase):
         self.regen_paddle_pressed
       )
 
-      # Always send PRNDL2 command when OpenPilot is in control
-      if regen_active:
-        prndl2_value = 7
-      else:
-        prndl2_value = 6
- 
+      prndl2_value = 7 if regen_active else 6
       regen_paddle_value = 2 if regen_active else 0
       manual_mode = 1 if prndl2_value == 7 else 0
- 
+
       can_sends.append(gmcan.create_prndl2_command(
         self.packer_pt, CanBus.POWERTRAIN, prndl2_value, manual_mode
       ))
-      can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN, regen_paddle_value))
+      can_sends.append(gmcan.create_regen_paddle_command(
+        self.packer_pt, CanBus.POWERTRAIN, regen_paddle_value
+      ))
 
 
     # Steering (Active: 50Hz, inactive: 10Hz)
