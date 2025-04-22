ee940118c47428c64d914e10bce8b7b80be2fc02
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-22 16:56:20 -0500

    Revert "25hz"
    This reverts commit 9d4da30d5dd6ba6d0926ececba7ef38671df1672.

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -122,7 +122,14 @@ class CarController(CarControllerBase):
     can_sends = []
 
 
-    if self.frame % 4 == 0:
+    # Send commands for PRNDL2 and regen paddle with unified timing
+    frames_since_last = self.frame - getattr(self, "last_trigger_frame_40hz", -4)
+    target_wait = 3 if getattr(self, "wait_long_40hz", False) else 2
+
+    if frames_since_last >= target_wait:
+      self.last_trigger_frame_40hz = self.frame
+      self.wait_long_40hz = not getattr(self, "wait_long_40hz", False)
+
       regen_active = (
         self.CP.carFingerprint in CC_REGEN_PADDLE_CAR and
         self.CP.openpilotLongitudinalControl and
