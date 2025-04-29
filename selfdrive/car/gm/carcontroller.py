d3aeed4249fad15681216e3efbf9d76feb6a8a11
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-29 14:54:07 -0500

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -135,27 +135,19 @@ class CarController(CarControllerBase):
       self.regen_paddle_pressed
     )
 
-    # Paddle must be sent at 40hz which clogs the bus and delays steer frames; Logic avoids steer frames to prevent LKAS faults
-    send_prndl_frame = (self.frame % 5) in (1, 3)
-    frames_since_last_steer = self.frame - getattr(self, "last_steer_frame", -100)
-    last_steer_time_ms = (now_nanos - CS.loopback_lka_steering_cmd_ts_nanos) * 1e-6
+    # Send regen paddle and PRNDL2 commands at ~40Hz using alternating 2/3 frame interval
+    frames_since_last = self.frame - getattr(self, "last_trigger_frame_40hz", -3)
+    target_wait = 3 if getattr(self, "wait_long_40hz", False) else 2
 
-    self.regen_ready_to_send = getattr(self, "regen_ready_to_send", False)
-
-    # If frame contains a steer command, wait to send
-    if regen_active and send_prndl_frame:
-      self.regen_ready_to_send = True
-
-    # Send at next available frame
-    if self.regen_ready_to_send:
-      self.last_prndl2_frame = self.frame
+    press_regen_paddle = None
+    if regen_active and frames_since_last >= target_wait:
+      self.last_trigger_frame_40hz = self.frame
+      self.wait_long_40hz = not getattr(self, "wait_long_40hz", False)
       press_regen_paddle = True
-      can_sends.append(gmcan.create_prndl2_command(self.packer_pt, CanBus.POWERTRAIN, press_regen_paddle))
-      can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN, press_regen_paddle))
-      self.regen_ready_to_send = False
-
     elif not regen_active and getattr(self, "last_regen_active", False):
       press_regen_paddle = False
+
+    if press_regen_paddle is not None:
       can_sends.append(gmcan.create_prndl2_command(self.packer_pt, CanBus.POWERTRAIN, press_regen_paddle))
       can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN, press_regen_paddle))
 
