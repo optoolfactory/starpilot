39584f76eb740cb568485151ecb7e0e906e4a4a2
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-05 00:38:38 -0500

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -121,8 +121,7 @@ class CarController(CarControllerBase):
     can_sends = []
 
     # Send regen paddle and PRNDL2 commands at 66Hz, avoiding steer frame timing
-    steer_phase = self.last_steer_frame % 3
-    send_prndl_frame = (self.frame % 3) != steer_phase
+    send_prndl_frame = self.frame != self.last_steer_frame
     press_regen_paddle = self.regen_paddle_pressed
 
     if (
