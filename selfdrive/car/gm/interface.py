6da1253dc4324fefad41e83de71f937103594353
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-12 14:23:23 -0500

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -147,7 +147,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_SDGM
 
     else:  # ASCM, OBD-II harness
-      ret.openpilotLongitudinalControl = True
+      ret.openpilotLongitudinalControl = not params.get_bool("DisableOpenpilotLongitudinal")
       ret.networkLocation = NetworkLocation.gateway
       ret.radarUnavailable = RADAR_HEADER_MSG not in fingerprint[CanBus.OBSTACLE] and not docs
       ret.pcmCruise = False  # stock non-adaptive cruise control is kept off
@@ -269,7 +269,7 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM
       ret.minEnableSpeed = -1
       ret.pcmCruise = False
-      ret.openpilotLongitudinalControl = True
+      ret.openpilotLongitudinalControl = not params.get_bool("DisableOpenpilotLongitudinal")
       ret.stoppingControl = True
       ret.autoResumeSng = True
 
@@ -293,7 +293,7 @@ class CarInterface(CarInterfaceBase):
       ret.radarUnavailable = True
       ret.experimentalLongitudinalAvailable = False
       ret.minEnableSpeed = 24 * CV.MPH_TO_MS
-      ret.openpilotLongitudinalControl = True
+      ret.openpilotLongitudinalControl = not params.get_bool("DisableOpenpilotLongitudinal")
       ret.pcmCruise = False
 
       if not ret.enableGasInterceptor and candidate in CC_ONLY_CAR: #redneck tuning
