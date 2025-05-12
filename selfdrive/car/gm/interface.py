b7948a7484cd4b88efe0ce13a2b6b213e35e344c
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-05-12 13:21:32 -0500

    Revert "Update interface.py"
    This reverts commit cac10dc58b7b2136ad192e21d5a8d69b4369c7e4.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -136,10 +136,6 @@ class CarInterface(CarInterfaceBase):
         ret.openpilotLongitudinalControl = True
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
 
-      # CAM_LONG flag only if camera-capable and conditional_experimental_mode enabled
-      if candidate in CAMERA_ACC_CAR and getattr(frogpilot_toggles, "conditional_experimental_mode", False):
-        ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
-
     elif candidate in SDGM_CAR:
       ret.longitudinalTuning.kiV = [0., 0., 0.]  # TODO: tuning
       ret.experimentalLongitudinalAvailable = False
@@ -293,14 +289,12 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in CC_ONLY_CAR:
       ret.flags |= GMFlags.CC_LONG.value
+      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_CC_LONG
       ret.radarUnavailable = True
       ret.experimentalLongitudinalAvailable = False
       ret.minEnableSpeed = 24 * CV.MPH_TO_MS
       ret.openpilotLongitudinalControl = True
       ret.pcmCruise = False
-
-      if not ret.enableGasInterceptor:
-        ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_CC_LONG
 
       if not ret.enableGasInterceptor and candidate in CC_ONLY_CAR: #redneck tuning
         ret.longitudinalTuning.kpBP = [10.7, 10.8, 28.]  # 10.7 m/s == 24 mph
