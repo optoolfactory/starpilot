d90fc18767bc86b12db302916255ca4d6561c6ae
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-11-04 16:01:24 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -269,6 +269,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
   });
   addItem(translateBtn);
 
+  /*
   QObject::connect(uiState(), &UIState::primeTypeChanged, [this] (PrimeType type) {
     pair_device->setVisible(type == PrimeType::UNPAIRED);
   });
@@ -279,6 +280,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
       }
     }
   });
+  */
 
   // power buttons
   QHBoxLayout *power_layout = new QHBoxLayout();
